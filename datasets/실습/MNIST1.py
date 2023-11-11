38317c61e7d6cf4c133c49ebe33b36d25fd959d9
youminki <108206432+youminki@users.noreply.github.com>
2023-11-11 18:48:03 +0900

    Commit

---
+++ datasets/실습/MNIST1.py
@@ -0,0 +1,51 @@
+
+(train_data, train_targets), (test_data, test_targets) =  boston_housing.load_data()
+from keras.datasets import boston_housing
+
+train_data.shape
+
+train_targets
+
+import numpy as np
+
+k = 4
+num_val_samples = len(train_data) // k
+num_epochs = 100
+all_scores = []
+for i in range(k):
+    print('처리중인 폴드 #', i)
+    # 검증 데이터 준비: k번째 분할
+    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+
+    # 훈련 데이터 준비: 다른 분할 전체
+    partial_train_data = np.concatenate(
+        [train_data[:i * num_val_samples],
+         train_data[(i + 1) * num_val_samples:]],
+        axis=0)
+    partial_train_targets = np.concatenate(
+        [train_targets[:i * num_val_samples],
+         train_targets[(i + 1) * num_val_samples:]],
+        axis=0)
+
+    # 케라스 모델 구성(컴파일 포함)
+    model = build_model()
+    # 모델 훈련(verbose=0 이므로 훈련 과정이 출력되지 않습니다)
+    model.fit(partial_train_data, partial_train_targets,
+                epochs=num_epochs, batch_size=1, verbose=0)
+    # 검증 세트로 모델 평가
+    val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
+    all_scores.append(val_mae)
+    
+    
+# train_images = train_images.reshape((60000, 784))
+# test_images = test_images.reshape((10000, 784))
+
+# print(train_imagesimages[1])
+
+
+train_labels, shape
+from tensorflow.keras.utils import to_categorical
+
+train_labels = to_categorical(train_labels)
+train_labels.shape
